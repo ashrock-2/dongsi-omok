b8fb738ad5bec187a4f3ee68037dc9a411a6b0f4
OnLee3 <dlwotjr2@gmail.com>
2024-08-10 07:40:30 +0900

    모노레포 구성

---
+++ back-end/index.js
@@ -0,0 +1,26 @@
+import { createServer } from "http";
+import { WebSocketServer } from "ws";
+
+const server = createServer();
+const wss = new WebSocketServer({ server });
+
+wss.on("connection", (ws) => {
+  console.log("New client connected");
+
+  ws.on("message", (message) => {
+    console.log("Received:", message.toString());
+    wss.clients.forEach((client) => {
+      if (client !== ws && client.readyState === client.OPEN) {
+        client.send(message);
+      }
+    });
+  });
+
+  ws.on("close", () => {
+    console.log("Client disconnected");
+  });
+});
+
+server.listen(8080, () => {
+  console.log("WebSocket server is listening on port 8080");
+});
