9f48b60b506ff4fb80f572c4d1a0a3456cdc6387
이재석 <jslee01@miridih.com>
2024-08-13 17:45:05 +0900

    mergePlaceAction 가구현

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -4,7 +4,7 @@ import { match } from "ts-pattern";
 import { WebSocketServer } from "ws";
 const server = createServer();
 const wss = new WebSocketServer({ server });
-const actions: Array<AnyActionType> = [];
+const actions: Array<ACTION_TYPE<'PLACE'>> = [];
 
 const isValidAction = (action: unknown): action is AnyActionType => {
   if (typeof action !== 'object' || action === null) return false;
@@ -17,13 +17,32 @@ const isValidAction = (action: unknown): action is AnyActionType => {
   return payload !== undefined;
 };
 
-const handleAction = (action: AnyActionType) => {
-  match(action).with({ id: 'PLACE'}, ({payload}) => {
-    wss.clients.forEach((client) => {
-      if (client.readyState === client.OPEN) {
-        client.send(JSON.stringify(action));
+const mergePlaceAction = (actions: Array<ACTION_TYPE<'PLACE'>>) => {
+  const [action1, action2] = actions;
+  if (action1.payload.row === action2.payload.row && action1.payload.col === action2.payload.col) {
+    const mergedAction: ACTION_TYPE<'PLACE'> = {
+      id: 'PLACE',
+      payload: {
+        item: 'prohibit',
+        row: action1.payload.row,
+        col: action1.payload.col,
       }
-    });
+    }
+    return [mergedAction];
+  }
+  return actions;
+};
+
+const handleAction = (action: AnyActionType) => {
+  match(action).with({ id: 'PLACE'}, (action) => {
+    actions.push(action);
+    if (actions.length === 2) {
+      wss.clients.forEach((client) => {
+          client.send(JSON.stringify(mergePlaceAction(actions)));
+      });
+      actions.pop();
+      actions.pop();
+    }
   }).exhaustive();
 }
 
