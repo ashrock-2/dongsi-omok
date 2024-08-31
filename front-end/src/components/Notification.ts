0b879cd85474213683789b7a9ba3a19a8451ebbd
OnLee3 <dlwotjr2@gmail.com>
2024-08-31 21:38:19 +0900

    template 이용하도록 변경

--- front-end/src/components/Notification.ts
+++ front-end/src/components/Notification.ts
@@ -3,45 +3,34 @@ import { State } from '../State';
 import type { GameState } from '@dongsi-omok/shared';
 import { applyParticleEffect } from './ParticleEffect';
 
+const template = document.createElement('template');
+template.innerHTML = `
+  <link rel="stylesheet" href="${new URL('./Notification.css', import.meta.url)}"></link>
+  <p>
+    <span></span>
+    <strong></strong>
+  </p>
+`;
+
 export class Notification extends HTMLElement {
   constructor() {
     super();
     this.attachShadow({ mode: 'open' });
-
-    const wrapper = document.createElement('p');
-    const style = document.createElement('style');
-    style.textContent = `
-      p {
-        text-align: center;
-        background-color: hsla(0, 0%, 0%, 25%);
-        padding: 8px 6px;
-        border-radius: 6px;
-        z-index: 1;
-        color: hsl(0, 47%, 94%);
-        position: fixed;
-        left: 50%;
-        top: 50%;
-        transform: translate(-50%, -50%);
-        cursor: pointer;
-        &:hover {
-          background-color: hsla(0, 0%, 0%, 42%);
-        }
-      }
-      strong {
-        font-size: 1.25rem;
-      }
-      `;
+    this.shadowRoot?.append(template.content.cloneNode(true));
+    const wrapper = this.shadowRoot?.querySelector('p') as HTMLParagraphElement;
+    const span = this.shadowRoot?.querySelector('span') as HTMLSpanElement;
+    const strong = this.shadowRoot?.querySelector('strong')!;
+    wrapper.style.display = getVisibility(State.gameState);
+    span.innerText = getSpanText(State);
+    strong.innerText = getStrongText(State);
+    State.addEventListener('stateChange', () => {
+      wrapper.style.display = getVisibility(State.gameState);
+      span.innerText = getSpanText(State);
+      strong.innerText = getStrongText(State);
+    });
     applyParticleEffect(wrapper);
-    this.shadowRoot?.append(style, wrapper);
-    updateTextAndVisibility(wrapper);
-    State.addEventListener('stateChange', () =>
-      updateTextAndVisibility(wrapper),
-    );
     wrapper.addEventListener('click', () => {
-      const textToCopy = wrapper.querySelector('strong');
-      if (textToCopy) {
-        copyText(textToCopy).then(() => this.showCopyComplete());
-      }
+      copyText(strong).then(() => this.showCopyComplete());
     });
   }
   private showCopyComplete() {
@@ -49,25 +38,19 @@ export class Notification extends HTMLElement {
   }
 }
 
-const updateTextAndVisibility = (dom: HTMLElement) => {
-  dom.innerHTML = getText(State);
-  dom.style.display = getVisibility(State.gameState);
-};
-
 const copyText = (dom: HTMLElement) =>
   new Promise<void>((resolve) =>
     navigator.clipboard.writeText(dom.innerText).then(() => resolve()),
   );
 
-const getText = (state: typeof State) =>
+const getSpanText = (state: typeof State) =>
   match(state)
     .with(
       {
         gameState: 'WAITING_FOR_OPPONENT',
         roomId: P.when((roomId) => roomId !== null),
       },
-      ({ roomId }) => `다음 주소를 상대방에게 공유해주세요.<br/>
-      <strong>https://dongsi-omok.vercel.app?roomId=${roomId}</strong>`,
+      () => '다음 주소를 상대방에게 공유해주세요.',
     )
     .with({ gameState: 'GAME_OVER' }, () => `다시 시작?`)
     .with(
@@ -76,6 +59,17 @@ const getText = (state: typeof State) =>
         roomId: P.when((roomId) => roomId === null),
       },
       () => `방을 생성 중입니다...`,
+    )
+    .otherwise(() => '');
+
+const getStrongText = (state: typeof State) =>
+  match(state)
+    .with(
+      {
+        gameState: 'WAITING_FOR_OPPONENT',
+        roomId: P.when((roomId) => roomId !== null),
+      },
+      ({ roomId }) => `https://dongsi-omok.vercel.app?roomId=${roomId}`,
     )
     .otherwise(() => '');
 
