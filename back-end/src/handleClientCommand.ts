b51fa7e455fdcb9867260534af9c5a88c64ca777
OnLee3 <dlwotjr2@gmail.com>
2024-09-10 07:00:54 +0900

    handleClientCommand 분리

---
+++ back-end/src/handleClientCommand.ts
@@ -0,0 +1,126 @@
+import {
+  initBoard,
+  makeServerCommand,
+  type ClientCommand,
+  type ClientCommandType,
+} from '@dongsi-omok/shared';
+import { match } from 'ts-pattern';
+import {
+  generateRoomId,
+  getCommandQueueState,
+  mergePlaceItemCommand,
+  updateBoardAndCheckWin,
+  type Rooms,
+} from './util';
+import type WebSocket from 'ws';
+
+export const handleClientCommand = (
+  command: ClientCommand,
+  ws: WebSocket,
+  rooms: Rooms,
+  clientMap: Map<WebSocket, string>,
+) => {
+  match(command)
+    .with({ id: 'PLACE_ITEM' }, (command: ClientCommandType<'PLACE_ITEM'>) => {
+      const roomId = clientMap.get(ws);
+      if (!roomId) return;
+      const room = rooms.get(roomId);
+      if (!room) return;
+      match({
+        queueState: getCommandQueueState(room.queue),
+        item: command.payload.item,
+      })
+        .with(
+          { queueState: 'EMPTY', item: 'black' },
+          { queueState: 'EMPTY', item: 'white' },
+          () => {
+            room.queue.push(command);
+          },
+        )
+        .with(
+          { queueState: 'BLACK', item: 'white' },
+          { queueState: 'WHITE', item: 'black' },
+          () => {
+            room.queue.push(command);
+            const placeItemCommand = mergePlaceItemCommand(room.queue);
+            const { isFinish, winner, winningCoordinates } =
+              updateBoardAndCheckWin(room.board, placeItemCommand);
+            room.clients.forEach((client) => {
+              client.send(JSON.stringify(placeItemCommand));
+              client.send(
+                JSON.stringify(
+                  makeServerCommand('NOTIFY_WINNER', {
+                    payload: { isFinish, winner, winningCoordinates },
+                  }),
+                ),
+              );
+            });
+            room.queue.length = 0;
+          },
+        )
+        .otherwise(() => {
+          // do nothing
+        });
+    })
+    .with({ id: 'CREATE_ROOM' }, () => {
+      const roomId = generateRoomId();
+      rooms.set(roomId, {
+        clients: [ws],
+        queue: [],
+        board: initBoard(),
+      });
+      clientMap.set(ws, roomId);
+      ws.send(
+        JSON.stringify(
+          makeServerCommand('SEND_ROOM_ID', { payload: { roomId } }),
+        ),
+      );
+    })
+    .with(
+      { id: 'JOIN_ROOM' },
+      ({ payload: { roomId } }: ClientCommandType<'JOIN_ROOM'>) => {
+        const room = rooms.get(roomId);
+        match(room)
+          .when(
+            (room) => room === undefined,
+            () => {
+              ws.send(JSON.stringify('no room with that id'));
+              ws.close();
+            },
+          )
+          .when(
+            (room) => room.clients.length === 1,
+            (room) => {
+              clientMap.set(ws, roomId);
+              room.clients.push(ws);
+              room.clients.forEach((ws, idx) => {
+                ws.send(
+                  JSON.stringify(
+                    makeServerCommand('SET_PLAYER_COLOR', {
+                      payload: { color: idx === 0 ? 'black' : 'white' },
+                    }),
+                  ),
+                );
+                ws.send(
+                  JSON.stringify(
+                    makeServerCommand('START_GAME', { payload: {} }),
+                  ),
+                );
+              });
+            },
+          )
+          .when(
+            (room) => room.clients.length >= 2,
+            () => {
+              ws.send(JSON.stringify('You are not allowed.'));
+              ws.close();
+            },
+          )
+          .otherwise(() => {
+            ws.send(JSON.stringify('You are not allowed.'));
+            ws.close();
+          });
+      },
+    )
+    .exhaustive();
+};
--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -1,137 +1,19 @@
-import {
-  type ClientCommandType,
-  type ClientCommand,
-  isValidClientCommand,
-  makeServerCommand,
-  initBoard,
-} from '@dongsi-omok/shared';
+import { isValidClientCommand, makeServerCommand } from '@dongsi-omok/shared';
 import { createServer } from 'http';
-import { match } from 'ts-pattern';
 import { WebSocketServer, WebSocket } from 'ws';
-import {
-  generateRoomId,
-  getCommandQueueState,
-  mergePlaceItemCommand,
-  updateBoardAndCheckWin,
-  type Rooms,
-} from './util';
+import { type Rooms } from './util';
+import { handleClientCommand } from './handleClientCommand';
 const server = createServer();
 const wss = new WebSocketServer({ server });
 const rooms: Rooms = new Map();
 const clientMap: Map<WebSocket, string> = new Map();
-
-const handleClientCommand = (command: ClientCommand, ws: WebSocket) => {
-  match(command)
-    .with({ id: 'PLACE_ITEM' }, (command: ClientCommandType<'PLACE_ITEM'>) => {
-      const roomId = clientMap.get(ws);
-      if (!roomId) return;
-      const room = rooms.get(roomId);
-      if (!room) return;
-      match({
-        queueState: getCommandQueueState(room.queue),
-        item: command.payload.item,
-      })
-        .with(
-          { queueState: 'EMPTY', item: 'black' },
-          { queueState: 'EMPTY', item: 'white' },
-          () => {
-            room.queue.push(command);
-          },
-        )
-        .with(
-          { queueState: 'BLACK', item: 'white' },
-          { queueState: 'WHITE', item: 'black' },
-          () => {
-            room.queue.push(command);
-            const placeItemCommand = mergePlaceItemCommand(room.queue);
-            const { isFinish, winner, winningCoordinates } =
-              updateBoardAndCheckWin(room.board, placeItemCommand);
-            room.clients.forEach((client) => {
-              client.send(JSON.stringify(placeItemCommand));
-              client.send(
-                JSON.stringify(
-                  makeServerCommand('NOTIFY_WINNER', {
-                    payload: { isFinish, winner, winningCoordinates },
-                  }),
-                ),
-              );
-            });
-            room.queue.length = 0;
-          },
-        )
-        .otherwise(() => {
-          // do nothing
-        });
-    })
-    .with({ id: 'CREATE_ROOM' }, () => {
-      const roomId = generateRoomId();
-      rooms.set(roomId, {
-        clients: [ws],
-        queue: [],
-        board: initBoard(),
-      });
-      clientMap.set(ws, roomId);
-      ws.send(
-        JSON.stringify(
-          makeServerCommand('SEND_ROOM_ID', { payload: { roomId } }),
-        ),
-      );
-    })
-    .with(
-      { id: 'JOIN_ROOM' },
-      ({ payload: { roomId } }: ClientCommandType<'JOIN_ROOM'>) => {
-        const room = rooms.get(roomId);
-        match(room)
-          .when(
-            (room) => room === undefined,
-            () => {
-              ws.send(JSON.stringify('no room with that id'));
-              ws.close();
-            },
-          )
-          .when(
-            (room) => room.clients.length === 1,
-            (room) => {
-              clientMap.set(ws, roomId);
-              room.clients.push(ws);
-              room.clients.forEach((ws, idx) => {
-                ws.send(
-                  JSON.stringify(
-                    makeServerCommand('SET_PLAYER_COLOR', {
-                      payload: { color: idx === 0 ? 'black' : 'white' },
-                    }),
-                  ),
-                );
-                ws.send(
-                  JSON.stringify(
-                    makeServerCommand('START_GAME', { payload: {} }),
-                  ),
-                );
-              });
-            },
-          )
-          .when(
-            (room) => room.clients.length >= 2,
-            () => {
-              ws.send(JSON.stringify('You are not allowed.'));
-              ws.close();
-            },
-          )
-          .otherwise(() => {
-            ws.send(JSON.stringify('You are not allowed.'));
-            ws.close();
-          });
-      },
-    )
-    .exhaustive();
-};
 
 wss.on('connection', (ws) => {
   ws.on('message', (message) => {
     try {
       const parsedMessage = JSON.parse(message.toString());
       if (isValidClientCommand(parsedMessage)) {
-        handleClientCommand(parsedMessage, ws);
+        handleClientCommand(parsedMessage, ws, rooms, clientMap);
       }
     } catch (err) {
       console.error('Failed to parse message', err);
