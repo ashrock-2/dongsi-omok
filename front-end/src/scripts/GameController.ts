d8e88081c3a4d5ec7739cd723c68a438e021b295
이재석 <jslee01@miridih.com>
2024-09-19 08:22:13 +0900

    handleServerCommand GameController로 통합

--- front-end/src/scripts/GameController.ts
+++ front-end/src/scripts/GameController.ts
@@ -1,12 +1,15 @@
 import { StateStore } from 'src/scripts/states/State';
 import { sendCommand, connectSSE } from 'src/scripts/utils/apiClient';
-import { handleServerCommand } from 'src/scripts/utils/handleServerCommand';
 import {
   makeClientCommand,
   isValidServerCommand,
   ALPHABETS,
+  type ServerCommand,
+  type ServerCommandType,
+  type Player,
 } from '@dongsi-omok/shared';
-import type { Board } from './main';
+import { match } from 'ts-pattern';
+import type { Board } from './components/Board';
 
 export class GameController {
   private readonly state: StateStore;
@@ -15,23 +18,14 @@ export class GameController {
   constructor(board: Board) {
     this.state = new StateStore();
     this.board = board;
-    this.board.addEventListener('click', (event) => {
-      const button = event.target as HTMLButtonElement;
-      const {
-        dataset: { row, col },
-      } = button;
-      if (!row || !col) {
-        return;
-      }
-      this.handleBoardClick(row, col);
-    });
+    this.board.addEventListener('click', this.handleBoardClick.bind(this));
     this.initSSE();
   }
 
   private async initSSE() {
     const eventSource = connectSSE((data) => {
       if (isValidServerCommand(data)) {
-        handleServerCommand(data, this.state);
+        this.handleServerCommand(data);
       }
     });
     return () => {
@@ -39,7 +33,11 @@ export class GameController {
     };
   }
 
-  public handleBoardClick(row: string, col: string) {
+  private handleBoardClick(event: Event) {
+    const button = event.target as HTMLButtonElement;
+    const { row, col } = button.dataset;
+    if (!row || !col) return;
+
     if (this.state.canPlaceItem()) {
       this.state.setAwaitMove();
       this.board.setItemOnBoard(row, col, 'plan', this.state.player!);
@@ -54,6 +52,71 @@ export class GameController {
         }),
       );
     }
+  }
+
+  private handleServerCommand(command: ServerCommand) {
+    match(command)
+      .with(
+        { id: 'PLACE_ITEM' },
+        ({ payload }: ServerCommandType<'PLACE_ITEM'>) => {
+          payload.forEach(({ item, row, col }) =>
+            this.board.setItemOnBoard(row, col, item, item as Player),
+          );
+          this.state.gameState = 'IN_PROGRESS';
+        },
+      )
+      .with(
+        { id: 'SET_PLAYER_COLOR' },
+        (command: ServerCommandType<'SET_PLAYER_COLOR'>) => {
+          this.state.player = command.payload.color;
+          console.log(`you are ${this.state.player}`);
+        },
+      )
+      .with(
+        { id: 'START_GAME' },
+        (command: ServerCommandType<'START_GAME'>) => {
+          this.state.playerId = command.payload.playerId;
+          this.state.gameState = 'IN_PROGRESS';
+        },
+      )
+      .with(
+        { id: 'SEND_ROOM_ID' },
+        (command: ServerCommandType<'SEND_ROOM_ID'>) => {
+          const { roomId } = command.payload;
+          this.state.roomId = roomId;
+        },
+      )
+      .with(
+        { id: 'NOTIFY_WINNER' },
+        (command: ServerCommandType<'NOTIFY_WINNER'>) => {
+          match(command.payload)
+            .with(
+              { isFinish: true, winner: 'black' },
+              { isFinish: true, winner: 'white' },
+              ({ winner, winningCoordinates }) => {
+                this.state.winner = winner;
+                this.state.gameState = 'GAME_OVER';
+                this.state.winningCoordinates = winningCoordinates!;
+                this.board.highlightWinningCoordinates(winningCoordinates!);
+              },
+            )
+            .with(
+              { isFinish: true, winner: null },
+              ({ winningCoordinates }) => {
+                this.state.gameState = 'GAME_OVER';
+                this.state.winningCoordinates = winningCoordinates!;
+                this.board.highlightWinningCoordinates(winningCoordinates!);
+              },
+            )
+            .otherwise(() => {
+              // do nothing
+            });
+        },
+      )
+      .with({ id: 'LEAVE_OPPONENT' }, () => {
+        this.state.gameState = 'LEAVE_OPPONENT';
+      })
+      .exhaustive();
   }
 
   public getState() {
