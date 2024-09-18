606a2882ca380f90df8e1eea606bfd7a8dd1595a
OnLee3 <dlwotjr2@gmail.com>
2024-09-18 19:00:33 +0900

    front 주소도 CORS 허가

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -1,18 +1,28 @@
-import { createServer } from 'http';
 import { type ClientMap, type GameQueue, type Rooms } from './util';
 import express from 'express';
 import cors from 'cors';
 import { handleCommand, handleSSEConnection } from './handlers';
 
 const app = express();
-const server = createServer(app);
 const rooms: Rooms = new Map();
 const clientMap: ClientMap = new Map();
 const gameQueue: GameQueue = [];
 
+const ALLOWED_ORIGINS = [
+  'http://localhost:4321',
+  'https://dongsi-omok.vercel.app',
+];
+
 app.use(
   cors({
-    origin: 'http://localhost:4321',
+    origin: function (origin, callback) {
+      // origin이 undefined인 경우는 같은 출처의 요청을 의미합니다.
+      if (!origin || ALLOWED_ORIGINS.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
     credentials: true,
     methods: ['GET', 'POST', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Client-ID'],
@@ -23,10 +33,11 @@ app.post('/api/command', (req, res) => {
   handleCommand(req, res, rooms, clientMap);
 });
 app.get('/api/events', (req, res) => {
+  console.log(rooms.size);
   handleSSEConnection(req, res, rooms, clientMap, gameQueue);
 });
 
 const PORT = Number(process.env.PORT) || 8080;
-server.listen(PORT, '0.0.0.0', () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is listening on port ${PORT}`);
 });
